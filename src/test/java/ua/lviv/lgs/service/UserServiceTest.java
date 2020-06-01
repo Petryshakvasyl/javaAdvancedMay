5d3bcb180fe1c6e74f1e4cb2ec902893e5534f4b
Vasyl Petryshak <vasylpetryshak@gmail.com>
2020-06-01 22:29:18 +0300

    add parametrized tests

--- src/test/java/ua/lviv/lgs/service/UserServiceTest.java
+++ src/test/java/ua/lviv/lgs/service/UserServiceTest.java
@@ -1,8 +1,10 @@
 package ua.lviv.lgs.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,13 +25,32 @@ class UserServiceTest {
 
     @Test
     void notFoundUserWithId() {
-        assertThrows(UserNotFoundException.class, () -> userService.notFoundUserWithId(1L));
+        Assertions.assertThrows(UserNotFoundException.class, () -> userService.notFoundUserWithId(1L));
     }
 
     @ParameterizedTest
     @EnumSource(Status.class)
     void parametrizedTest(Status status) {
+        System.out.println(status);
         assertNotNull(status);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Status.class, names = {"NEW", "ACTIVE"})
+    void parametrizedTestForFewEnums(Status status) {
+        System.out.println(status);
+        assertNotNull(status);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Status.class, names = {"NEW", "ACTIVE"})
+    @CsvSource({"a1, b1, c1",
+            "a2, b2, c2"
+    })
+    void parametrizedTestForFewEnums(String a, String b, String c) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 
     enum Status{
