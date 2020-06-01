55cb784edc8c98ac3613bc1654bdc8e6f8534fa9
Vasyl Petryshak <vasylpetryshak@gmail.com>
2020-06-01 22:35:22 +0300

    add project info

---
+++ READMY.md
@@ -0,0 +1,2 @@
+# Java advanced course
+* [lesson-02](https://github.com/Petryshakvasyl/javaAdvancedMay/tree/lesson-2-junit)  (Junit)
--- src/main/java/ua/lviv/lgs/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package ua.lviv.lgs.service;
-
-public class UserService {
-
-    public String getAdminName() {
-        return "Admin";
-    }
-}
